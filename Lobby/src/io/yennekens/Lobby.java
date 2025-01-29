a7eb2bbde1756def75c97d01eebc1eec3fb611ec
Yaro Vennekens <yennekens@MacBook-Air-van-Yaro.local>
2025-01-29 14:14:16 +0100

    working base

--- Lobby/src/io/yennekens/Lobby.java
+++ Lobby/src/io/yennekens/Lobby.java
@@ -1,4 +1,33 @@
 package io.yennekens;
 
-public class Lobby {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lobby extends JavaPlugin
+{
+
+    public PluginManager pluginManager;
+
+
+    public void onEnable(){
+        pluginManager = Bukkit.getPluginManager();
+
+        registerCommands();
+        registerEvents(pluginManager);
+    }
+
+    public void onDisable(){
+
+
+    }
+
+    public void registerCommands(){
+
+    }
+
+    public void registerEvents(PluginManager pluginManager){
+
+    }
+
 }
